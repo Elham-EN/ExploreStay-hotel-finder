70f56fd67f9ef634aff129d666070d9153d189fa
Elham-EN <ilhamrezaie@gmail.com>
2024-03-16 23:59:29 +1100

    setup cross env

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,6 +4,7 @@ export default async function connectDatabase(connection: string) {
   try {
     await mongoose.connect(connection);
     console.log("Connected to mongoDB database");
+    console.log("Connection String:", connection);
   } catch (error) {
     console.log("Failed to connect to the database", error);
     // process.exit(1); // Exit app with a failure code
